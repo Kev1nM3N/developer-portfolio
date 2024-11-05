7fa4344f40ff10bce5268ef95e567a0c8563d088
Kevin Mendez <mediaman502@aol.com>
2024-11-05 18:27:50 -0500

    template is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/iconMonitor.png" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
